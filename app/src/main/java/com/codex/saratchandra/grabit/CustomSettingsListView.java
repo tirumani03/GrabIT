ced20019269b865a17fb9c80bfc7ff42e5e3311f
tirumani03 <chandra.sarat03@gmail.com>
2016-06-26 16:04:21 +0530

    First version.

--- app/src/main/java/com/codex/saratchandra/grabit/CustomSettingsListView.java
+++ app/src/main/java/com/codex/saratchandra/grabit/CustomSettingsListView.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 public class CustomSettingsListView extends ArrayAdapter<String>{
     private final Activity context;
     private final String[] web;
